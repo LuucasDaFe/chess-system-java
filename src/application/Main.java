e145168cb44f3d1c2f0ac1b8985baa7ada3b9af9
Lucas Abraao <Labraao957@gmail.com>
2023-12-03 10:27:41 -0300

    First package: chess

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,7 @@
 package application;
 
 import boardGame.Board;
-import boardGame.Position;
+import chess.ChessMatch;
 
 public class Main {
     public Main() {
@@ -9,7 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Board board = new Board(8, 8);
-        
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
